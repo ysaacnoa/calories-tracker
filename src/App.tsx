f78941d44dc8e0aea83c708de14ce938deceefc4
LilAtza <ysaacnoe.correa@unmsm.edu.pe>
2024-04-24 20:56:23 -0500

    ♻️ (app): principal body and From import

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
+import Form from "./components/Form"
 
 
 function App() {
 
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <>
+      <header className="bg-lime-600 py-3">
+        <div className="max-w-4xl mx-auto flex justify-between">
+          <h1 className="text-center text-lg font-black text-white uppercase">
+            Contador de calorias
+          </h1>
+        </div>
+      </header>
+
+      <section className="bg-lime-500 py-20 px-5">
+        <div className="max-w-4xl mx-auto">
+          <Form/>
+        </div>
+      </section>
+    </>
   )
 }
 
